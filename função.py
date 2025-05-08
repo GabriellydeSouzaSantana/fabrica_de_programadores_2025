cec25b6f809a7003fed4362eca5b83d5aed4698a
GabriellydeSouzaSantana <gabriellydesouzasantana@gmail.com>
2025-05-08 15:52:51 -0300

    Códigos função
    Códigos função fabrica de programadores

---
+++ função.py
@@ -0,0 +1,39 @@
+texto_escrito = "este livro é legal"
+outro_texto ='vou aprender a programar'
+outro_texto = texto_escrito
+
+#print(outro_texto)
+primeiro_nome = "gabrielly"
+segundo_nome = "santana"
+nome_completo = primeiro_nome + segundo_nome 
+
+saudacao= "ola "+"gabrielly" + segundo_nome 
+#print(saudacao)
+
+balas_subtraidas = 3
+quantidades_balas = 20
+quantidades_balas -= balas_subtraidas
+#print(quantidades_balas) 
+
+
+balas_divididas = 3 
+quantidades_balas = 20
+quantidades_balas /= balas_divididas 
+#print(quantidades_b
+
+
+nome = input()
+saudacao = "ola "+nome
+#print(saudacao)
+
+numero_string = input()
+numero_inteiro = int ( numero_string )
+subtracao = numero_inteiro - 2 
+
+
+numero_um = input()
+numero_dois = input()
+resultado_soma = int(numero_um) + int( numero_dois)
+resultado_soma_texto = str(resultado_soma)
+mensagem = "o resultado da soma é: "+resultado_soma_texto
+print(mensagem)
