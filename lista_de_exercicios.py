813e8f5c4539c87c997cc1a809d8fb4581854031
GabriellydeSouzaSantana <gabriellydesouzasantana@gmail.com>
2025-05-09 15:56:52 -0300

    Lista de exercícios
    Exercícios realizados na fabrica de programadores

---
+++ lista_de_exercicios.py
@@ -0,0 +1,9 @@
+valor = input()
+#valor_calculado = +int(valor) ** 3 
+#apresentacao = "a area do quadrado é: " + str(valor_calculado)
+#print(apresentacao)
+
+salariopreco = input ("me informe o salario: ")
+calculo_reajuste = 0.15 * int(salariopreco) + int(salariopreco)
+reajuste_salario = "seu salario foi reajustado para " + str(calculo_reajuste)+("e teve um reajuste de 15%")
+print(reajuste_salario)
