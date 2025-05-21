3827b184d4a4a8344b2914e096043806a428c453
GabriellydeSouzaSantana <gabriellydesouzasantana@gmail.com>
2025-05-21 15:54:22 -0300

    Tabuada
    Exercícios de tabuada realizados no dia 21/05/2025 na fabrica de programadores

---
+++ tabuada.py
@@ -0,0 +1,38 @@
+#n = int(input("Tabuada de:"))
+#x = 1 
+#while x<=10:
+    #print(n*x)
+    #x=x=1
+
+
+
+#Ex5.6
+#n = int(input("Tabuada de:"))
+#while 
+#x<=10:
+    #print(n,"x", x, "= ", n*x)
+    #x=x+1
+
+
+
+
+#Ex5.7
+#def tabuada():
+    #n1 = int(input("digite o numero que voce quer multiplicar: "))
+    #n2 = int(input("digite até onde vai a tabuada:"))
+    #while n1<=n2:
+        #print((n1), "x", (n2), "=", n1*n2)
+        #n1=n2+1
+#tabuada()
+
+
+
+
+S=0
+while True:
+    V=int(input("digite o numero a somar ou 0 para sair: "))
+    if V==0:
+        break
+    S = S+V
+print(S)
+
