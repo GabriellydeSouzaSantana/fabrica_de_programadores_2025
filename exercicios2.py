4c880854d5672523a6506ef965b624979491ddec
GabriellydeSouzaSantana <gabriellydesouzasantana@gmail.com>
2025-05-27 15:50:12 -0300

    Exerecicios2.py
    Exercícios realizados no dia 27/05, fabrica de programadores

---
+++ exercicios2.py
@@ -0,0 +1,37 @@
+#Exc9
+#def numerosinteiros():
+    #num1 = int(input("digite um valor: "))
+    #num2 = int(input("digite o segundo valor: "))
+    #print("a soma dos quardados é: " num1+num2*num1=num2)
+#numerosinteiros()
+
+#Exc10
+#def diferençasvalores():
+    #n1 = float(input("digite um valor: "))
+    #n2 = float(input("digite o segundo valor: "))
+    #if n1 > n2:
+        #print( "a diferença de valores é:", n1-n2) 
+    #else:
+        #print("a diferença de valores é de: ", n2-n1)
+#diferençasvalores()
+
+
+
+#Exc11
+#import math
+
+#def raiocircun():
+    #r = float(input("digite o valor do raio de uma circunferencia: "))
+    #c = (2*math.pi*r)
+    #calculado = ("O comprimento da circuferencia é de:") + str(c)
+    #print(calculado)
+
+#raiocircun()
+
+
+
+#Exc12
+def idadefutura():
+   idadeatual= int(input("Digite sua idade hoje: "))
+   print("sua idade daqui a 17 anos será: , idadeatual+17")
+idadefutura()
